ec81c7cb43d37b997cecbee6d4a3b65015053e13
CamiloVega2 <vegajuan208@gmail.com>
2023-02-17 13:52:19 -0500

    triki

---
+++ dom/main3.js
@@ -0,0 +1,17 @@
+const triki =  [linea1,linea2,linea3]
+const linea1 = []
+const linea2 = []
+const linea3 = []
+const caja1 = document.getElementById('cajita1').value
+const caja2 = document.getElementById('cajita2').value    
+const caja3 = document.getElementById('cajita3').value
+const caja4 = document.getElementById('cajita4').value    
+const caja5 = document.getElementById('cajita5').value    
+const caja6 = document.getElementById('cajita6').value    
+const caja7 = document.getElementById('cajita7').value
+const caja8 = document.getElementById('cajita8').value
+const caja9 = document.getElementById('cajita9').value
+
+function ponericono(){
+    
+}
