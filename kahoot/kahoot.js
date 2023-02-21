b49947dff72d963fb9dd644827afae673dcce425
CamiloVega2 <vegajuan208@gmail.com>
2023-02-21 13:57:36 -0500

    kahoot

---
+++ kahoot/kahoot.js
@@ -0,0 +1,93 @@
+const preguntas = {
+  preguntas: [
+    {
+      pregunta: "¿Qué videojuego fue primero?",
+      opciones: {
+        a: "Pac-man",
+        b: "Tetris",
+        c: "Pong",
+        d: "Donkey Kong",
+      },
+      respuesta_correcta: "c",
+    },
+    {
+      pregunta: "¿Cómo se llama el hermano de Mario Bros?",
+      opciones: {
+        a: "Luigi",
+        b: "Mario Jr.",
+        c: "Honguito",
+        d: "Luis",
+      },
+      respuesta_correcta: "a",
+    },
+    {
+      pregunta: "¿Cuántos luchadores hay en Street Figther II?",
+      opciones: {
+        a: "12",
+        b: "8",
+        c: "4",
+        d: "16",
+      },
+      respuesta_correcta: "a",
+    },
+    {
+      pregunta: "¿Cuántas entradas para mandos tiene Nintendo 64?",
+      opciones: {
+        a: "2",
+        b: "1",
+        c: "4",
+        d: "6",
+      },
+      respuesta_correcta: "c",
+    },
+    {
+      pregunta:
+        "¿Cuál es el material de la primera espada que recibes en The Legend of Zelda?",
+      opciones: {
+        a: "Hueso",
+        b: "Madera",
+        c: "Hierro",
+        d: "Acero",
+      },
+      respuesta_correcta: "b",
+    },
+  ],
+};
+console.log(preguntas);
+const pregunta1 = preguntas.preguntas[0].pregunta
+const opciona1 = preguntas.preguntas[0].opciones.a
+const opcionb1 = preguntas.preguntas[0].opciones.b
+const opcionc1 = preguntas.preguntas[0].opciones.c
+const opciond1 = preguntas.preguntas[0].opciones.d
+const respuesta_correcta1 = preguntas.preguntas[0].opciones.c
+
+const pregunta2 = preguntas.preguntas[1].pregunta
+const opciona2 = preguntas.preguntas[1].opciones.a
+const opcionb2 = preguntas.preguntas[1].opciones.b
+const opcionc2 = preguntas.preguntas[1].opciones.c
+const opciond2 = preguntas.preguntas[1].opciones.d
+const respuesta_correcta2 = preguntas.preguntas[1].opciones.a
+
+const pregunta3 = preguntas.preguntas[2].pregunta
+const opciona3 = preguntas.preguntas[2].opciones.a
+const opcionb3 = preguntas.preguntas[2].opciones.b
+const opcionc3 = preguntas.preguntas[2].opciones.c
+const opciond3 = preguntas.preguntas[2].opciones.d
+const respuesta_correcta3 = preguntas.preguntas[2].opciones.a
+
+const pregunta4 = preguntas.preguntas[3].pregunta
+const opciona4 = preguntas.preguntas[3].opciones.a
+const opcionb4 = preguntas.preguntas[3].opciones.b
+const opcionc4 = preguntas.preguntas[3].opciones.c
+const opciond4 = preguntas.preguntas[3].opciones.d
+const respuesta_correcta4 = preguntas.preguntas[3].opciones.c
+
+const pregunta5 = preguntas.preguntas[4].pregunta
+const opciona5 = preguntas.preguntas[4].opciones.a
+const opcionb5 = preguntas.preguntas[4].opciones.b
+const opcionc5 = preguntas.preguntas[4].opciones.c
+const opciond5 = preguntas.preguntas[4].opciones.d
+const respuesta_correcta5 = preguntas.preguntas[4].opciones.b
+
+
+
