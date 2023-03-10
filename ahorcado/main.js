f6f50bc8efdb5074cc5c3c944e674119cff6e06d
CamiloVega2 <vegajuan208@gmail.com>
2023-03-10 13:54:36 -0500

    ahorcado

--- ahorcado/main.js
+++ ahorcado/main.js
@@ -1,4 +1,5 @@
 const texto = document.getElementById("cajadetexto");
+const cajagrande = document.getElementById("cajagrande");
 const btn = document.getElementById("btn");
 const palabra = document.getElementById("palabra");
 const palabras = [
@@ -22,15 +23,19 @@ let añadirletra = document.getElementById("input");
 let letras = [];
 let contletras = 1;
 let listadeletras = document.getElementById("texto");
+let ganador1 = palabraahorcado.split("", palabraahorcado.length);
+let ganador = Array.apply("", Array(palabraahorcado.length));
 let contwin = 0;
+let contloss = 0;
 
 for (let i = 0; i < lineas; i++) {
-  palabra.innerHTML += `<div id="letra${i}" class = "text-4xl flex justify-center items-center border-solid border-2 border-black border-t-transparent border-r-transparent border-l-transparent h-[5rem] w-full"></div>`;
+  palabra.innerHTML += `<div id="letra${i}" class = "text-gray-200 text-4xl flex justify-center items-center border-solid border-2 border-gray-200 border-t-transparent border-r-transparent border-l-transparent h-[5rem] w-full"></div>`;
 }
 function verificarletra() {
   for (let j = 0; j < palabraahorcado.length; j++) {
     if (texto.value == "" || !isNaN(texto.value)) {
       alert("ingrese una letra");
+      contletras -= 1
       break;
     } else {
       if (
@@ -41,19 +46,66 @@ function verificarletra() {
         if (hola >= 0) {
           añadirletra = document.getElementById(`letra${hola}`);
           añadirletra.innerHTML = `${texto.value}`;
-          hola = "";
           listadeletras = document.getElementById(`usada${contletras}`);
           listadeletras.innerHTML = `${texto.value}`;
+          ganador.splice(hola, 1, texto.value);
+          hola = "";
         }
       } else {
         if (letras.includes(texto.value)) {
           alert("esta letra ya la uso");
           break;
         } else {
-          alert("esta letra no esta en la palabra");
-          hola = "";
-          listadeletras = document.getElementById(`usada${contletras}`);
-          listadeletras.innerHTML = `${texto.value}`;
+          contloss += 1;
+          switch (contloss) {
+            case 1:
+              cajagrande.innerHTML = `<img class="w-full border-solid border-4 border-gray-400 rounded-[300px]" src="img/img1.png">`;
+              alert("esta letra no esta en la palabra");
+              hola = "";
+              listadeletras = document.getElementById(`usada${contletras}`);
+              listadeletras.innerHTML = `${texto.value}`;
+              break;
+            case 2:
+              cajagrande.innerHTML = `<img class="w-full border-solid border-4 border-gray-400 rounded-[300px]" src="img/img2.png">`;
+              alert("esta letra no esta en la palabra");
+              hola = "";
+              listadeletras = document.getElementById(`usada${contletras}`);
+              listadeletras.innerHTML = `${texto.value}`;
+              break;
+            case 3:
+              cajagrande.innerHTML = `<img class="w-full border-solid border-4 border-gray-400 rounded-[300px]" src="img/img3.png">`;
+              alert("esta letra no esta en la palabra");
+              hola = "";
+              listadeletras = document.getElementById(`usada${contletras}`);
+              listadeletras.innerHTML = `${texto.value}`;
+              break;
+            case 4:
+              cajagrande.innerHTML = `<img class="w-full border-solid border-4 border-gray-400 rounded-[300px]" src="img/img4.png">`;
+              alert("esta letra no esta en la palabra");
+              hola = "";
+              listadeletras = document.getElementById(`usada${contletras}`);
+              listadeletras.innerHTML = `${texto.value}`;
+              break;
+            case 5:
+              cajagrande.innerHTML = `<img class="w-full border-solid border-4 border-gray-400 rounded-[300px]" src="img/img5.png">`;
+              alert("esta letra no esta en la palabra");
+              hola = "";
+              listadeletras = document.getElementById(`usada${contletras}`);
+              listadeletras.innerHTML = `${texto.value}`;
+              break;
+            case 6:
+              cajagrande.innerHTML = `<img class="w-full border-solid border-4 border-gray-400 rounded-[300px]" src="img/img6.png">`;
+              alert("esta letra no esta en la palabra");
+              hola = "";
+              listadeletras = document.getElementById(`usada${contletras}`);
+              listadeletras.innerHTML = `${texto.value}`;
+              break;
+          }
+          if (contloss == 6) {
+            alert("perdio papus");
+            break;
+          }
+
           break;
         }
       }
@@ -62,21 +114,19 @@ function verificarletra() {
   letras.push(texto.value);
   texto.value = "";
   contletras += 1;
-}
-function ganador() {
   for (let k = 0; k < palabraahorcado.length; k++) {
-    if (isNaN(document.getElementById(`letra${k}`).value)) {
-      contwin += 1;
-    } else {
+    if (ganador[k] != ganador1[k]) {
+      contwin = 0;
       break;
+    } else {
+      contwin += 1;
+      if (contwin == palabraahorcado.length) {
+        alert("gano papus");
+      }
     }
   }
-  if (contwin == palabraahorcado.length) {
-    alert("ha ganado wey");
-  }
+  console.log(ganador);
+  console.log(ganador1);
+  console.log(contwin);
 }
-
-btn.addEventListener("click", () =>{
-  verificarletra();
-  ganador();
-});
+btn.addEventListener("click", verificarletra);
